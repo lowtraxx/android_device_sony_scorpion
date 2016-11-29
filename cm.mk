916ee6839b508904bb18aafff5eea04f0d856957
Oliver Pahl <oliver.pahl@googlemail.com>
2016-11-29 16:44:21 +0100

    Removing obsolete nfc-enhanced makefile

--- cm.mk
+++ cm.mk
@@ -11,9 +11,6 @@ $(call inherit-product, device/sony/shinano/platform2.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Fingerprint for honami (from stock)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP621
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP621/SGP621:6.0.1/23.5.A.1.291/3706784398:user/release-keys
